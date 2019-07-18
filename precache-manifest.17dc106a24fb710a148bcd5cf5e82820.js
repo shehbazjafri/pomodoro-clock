3e01c9a36281df5026f3f8056f2176f9d90f2a39
shehbazjafri <galaxytwirls@gmail.com>
2019-07-18 17:02:42 +0530

    Updates

---
+++ precache-manifest.17dc106a24fb710a148bcd5cf5e82820.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd3e735852931d8df3d5",
+    "url": "/pomodoro-clock/static/css/main.7d3713dd.chunk.css"
+  },
+  {
+    "revision": "bd3e735852931d8df3d5",
+    "url": "/pomodoro-clock/static/js/main.bd3e7358.chunk.js"
+  },
+  {
+    "revision": "3cf2aa3977b110621c10",
+    "url": "/pomodoro-clock/static/js/1.3cf2aa39.chunk.js"
+  },
+  {
+    "revision": "f41d346ce9ebc121a229",
+    "url": "/pomodoro-clock/static/js/runtime~main.f41d346c.js"
+  },
+  {
+    "revision": "1c00436804eec00eb2ce4249529ecbef",
+    "url": "/pomodoro-clock/static/media/beep1.1c004368.mp3"
+  },
+  {
+    "revision": "d665ec66b496e96689cba724723e3689",
+    "url": "/pomodoro-clock/index.html"
+  }
+];
